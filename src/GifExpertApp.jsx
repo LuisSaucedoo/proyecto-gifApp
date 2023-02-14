39bebe09eeee7bc64dde01874af355fa58e50e02
Luis Saucedo <luigisaucedo.ls@gmail.com>
2023-02-14 13:09:53 -0400

    Agregando una nueva categoría

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,9 +5,16 @@ export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['ejemplo1', 'ejemplo2']);
 
+    const onAddCategory = () => {
+        setCategories([ 'Valorant', ...categories ]);
+        // setCategories( categories => [...categories, 'Valorant']);
+    }
+
     return(
         <>
             <h1>GifExpertApp</h1>
+
+            <button onClick={onAddCategory}>Agregar</button>
 
             <ol>
                 { categories.map( category => {
