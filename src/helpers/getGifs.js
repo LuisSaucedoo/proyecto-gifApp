4a523086471315399f0e8969741ea8ef50a59b25
Luis Saucedo <luigisaucedo.ls@gmail.com>
2023-02-15 13:23:21 -0400

    useEffect - Probando su funcionalidad

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 // Función - pésima práctica
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=HpDBGIIJjESbTYJqsUVyZxikiNXoeuyU&q=${ category }&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=HpDBGIIJjESbTYJqsUVyZxikiNXoeuyU&q=${ category }&limit=10`;
     const respuesta = await fetch( url );
     const { data } = await respuesta.json();
 
