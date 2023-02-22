686e21d670e40dfb65c7f514f5879d903a197b75
Luis Saucedo <luigisaucedo.ls@gmail.com>
2023-02-22 08:48:48 -0400

    Archivos de barril - importaciones más eficientes

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,14 +1,12 @@
 
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import { AddCategory, GifGrid } from "./components";
 
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['Messi']);
 
     const onAddCategory = ( newCategory ) => {
-
         if( categories.includes(newCategory) ) return;
         setCategories([ newCategory, ...categories ]);
     }
